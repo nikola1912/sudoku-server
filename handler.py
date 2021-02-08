e892185f679d3d0f0f1f981fa3e43dd5c7158e20
Nikola Jovanovic <nikola.jov1912@gmail.com>
2021-02-08 01:43:44 +0100

    Create initial sudoku scanner lambda

--- handler.py
+++ handler.py
@@ -1,19 +1,23 @@
-# def scan_sudoku():
-#     image = request.files['image']
-#     print(image)
-#     return {
-#         "sudoku": []
-#     }
-
 import json
+import base64
+import boto3
+from requests_toolbelt.multipart import decoder
+
+client = boto3.client('rekognition')
 
 
-def hello(event, context):
-    body = {
-        "message": "Hello there!",
-    }
+def scan_sudoku(event, context):
+    base64_string = event['body']
+    content_type = event['headers']['Content-Type']
+
+    multipart_string = base64.b64decode(base64_string)
+    multipart_data = decoder.MultipartDecoder(multipart_string, content_type)
+    file_data = multipart_data.parts[0].content
+
+    response = client.detect_text(Image={'Bytes': file_data})
+    print(response)
 
     return {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "body": "Sudoku successfully scanned"
     }
